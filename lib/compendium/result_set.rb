acddc913a4aa56053a89b9ea2be412cbff197924
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2018-01-26 13:11:17 -0500

    Add rubocop, fix offenses

--- lib/compendium/result_set.rb
+++ lib/compendium/result_set.rb
@@ -3,10 +3,11 @@ require 'active_support/core_ext/hash/indifferent_access'
 
 module Compendium
   class ResultSet
-    delegate :first, :last, :to_a, :empty?, :each, :map, :inject, :select, :detect, :[], :count, :length, :size, :==, to: :records
+    include Enumerable
+    delegate :each, :empty?, :length, :size, :==, to: :records
 
     attr_reader :records
-    alias :all :records
+    alias_method :all, :records
 
     def initialize(records)
       @records = if records.respond_to?(:map)
@@ -26,7 +27,7 @@ module Compendium
 
     def as_json(options = {})
       return records unless records.first.respond_to?(:except)
-      records.map{ |r| r.except(*options[:except]) }
+      records.map { |r| r.except(*options[:except]) }
     end
   end
 end
